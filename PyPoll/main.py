b1f50fc5899079444cedff555936af63a8f1f361
sabrinasaleh <sabrinafsaleh@gmail.com>
2020-09-03 04:11:42 +0000

    made progress in PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,10 +5,11 @@ import csv
 path_1 = os.path.join("Resources", "election_data.csv")
 
 votes = []
-# candidate_list = []
-# percent_votes = []
-# number_votes = []
-# winner = []
+candidates = []
+unique_candidate = []
+vote_count = []
+vote_percent = []
+
 
 with open(path_1, "r") as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -16,22 +17,42 @@ with open(path_1, "r") as csvfile:
         
     for row in csv_reader:
         votes.append(row[0])
+        candidates.append(row[2])
+        
+# total number of votes        
         count_votes = len(votes)
+        
+        
+    for x in set(candidates):
+        unique_candidate.append(x)
+        # y is the total number of votes per candidate
+        y = candidates.count(x)
+        vote_count.append(y)
+        # z is the percent of total votes per candidate
+        z = (y/count_votes)*100
+        vote_percent.append(z)
+        
+        
+#         winner = unique_candidate[vote_count.index(max(vote_count))+1]
+        
+    
+    
+    
+
+
+
+
         
       
     
-output = ( 
-    f"Election Results\n"
-    f"-----------------------------\n"
-    f"Total Votes: {count_votes}\n"
-#     f"-----------------------------\n"
-#     f"Khan: {}\n"
-#     f"Correy: {}\n"
-#     f"Li: {}\n"
-#     f"O'Tooley: {}\n"
-#     f"-----------------------------\n"
-#     f"Winner: {}\n"
-#     f"-----------------------------\n"
-    )
- 
-print(output)
+
+print(f"Election Results")
+print(f"-----------------------------")
+print(f"Total Votes: {count_votes}")
+
+for i in range(len(unique_candidate)):
+    print(f"{unique_candidate[i]}: {vote_percent[i]}% ({vote_count[i]})")
+    
+# print(f"Winner: {winner}")
+print(f"-----------------------------")
+   
